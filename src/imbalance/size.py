995c2f8c1c8b01390fc361de0f39c7b36fbf313a
tappasarn <time723@gmail.com>
2019-09-12 13:30:34 +0700

    make direct return

--- src/imbalance/size.py
+++ src/imbalance/size.py
@@ -13,6 +13,4 @@ def random_cluster_sizes(n, num_terms = None):
     num_terms = (num_terms or r.randint(2, n)) - 1
     a = r.sample(range(1, n), num_terms) + [0, n]
     list.sort(a)
-    x = np.array([a[i+1] - a[i] for i in range(len(a) - 1)])
-    print(x)
-    return x
+    return np.array([a[i+1] - a[i] for i in range(len(a) - 1)])
