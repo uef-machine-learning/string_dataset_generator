15ff20d12c21b569ff2d532c00715e767b3f2186
tappasarn <time723@gmail.com>
2019-09-12 14:03:08 +0700

    remove union calculation to optimize the speed

--- src/distances/jaccard.py
+++ src/distances/jaccard.py
@@ -8,11 +8,10 @@ y : (numpy array) second string sequence
 return:
 (float) : Jaccard's distance socre (1 - Jaccard's similarlity)
 """
+
 def _get_number_of_intersec_data(x, y):
     return len(set(x) & set(y))
 
-def _get_number_of_union_data(x, y):
-    return len(set(x) | set(y))
-
 def jaccard_seq(x, y):
-    return 1 - (_get_number_of_intersec_data(x, y) / _get_number_of_union_data(x, y))
+    num_intersect = _get_number_of_intersec_data(x,y)
+    return 1 - (num_intersect / (len(x) + len(y) - num_intersect))
