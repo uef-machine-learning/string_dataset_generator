0528e50456d14943cd347b6cc88f14748c79ff1b
tappasarn <time723@gmail.com>
2019-09-17 17:19:29 +0700

    fix bug

--- src/distances/jaccard_cy.pyx
+++ src/distances/jaccard_cy.pyx
@@ -1,5 +1,6 @@
 cpdef float jaccard_seq(list x, list y):
-    cdef int len_x, len_y, num_intersect
+    cdef int len_x, len_y
+    cdef float num_intersect
     cdef list fst, snd
     
     len_x = len(x)
