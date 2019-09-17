0ef26bf828a0037a89031131eba69fcee2e8a558
tappasarn <time723@gmail.com>
2019-09-17 14:29:28 +0700

    stop using cython for now

--- src/entries/entry.py
+++ src/entries/entry.py
@@ -26,7 +26,7 @@ APIs
 import numpy as np
 import random
 import math
-from distances import jaccard_cy as jaccard
+from distances import jaccard
 
 DEFAULT_STRING = '   '
 
@@ -147,7 +147,7 @@ def generate_cluster_members(
                 size_of_set[1], 
                 distance_threshold)
 
-            if jaccard.jaccard_seq(list(representatives[i]), list(member)) < distance_threshold:
+            if jaccard.jaccard_seq(representatives[i], member) < distance_threshold:
                 np.random.shuffle(member)
                 data.append(member)
                 ground_truths.append(i)
