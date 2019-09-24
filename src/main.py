8b01d11bdc05f3e951043a923a7971b4f616a14a
tappasarn <time723@gmail.com>
2019-09-24 15:18:54 +0700

    change out file name format

--- src/main.py
+++ src/main.py
@@ -14,10 +14,11 @@ DATA_SIZE = 1000
 NUMBER_OF_CLUSTER = 6
 SIZE_OF_CLUSTERS = [] #size.random_cluster_sizes(DATA_SIZE, NUMBER_OF_CLUSTER)
 DIMENSION = 200
-DISTANCE_THRESHOLD = 0.78
+DISTANCE_THRESHOLD = 0.85
 SIZE_OF_SET = (4,20)
 ALL_FEATURE_FILE_PATH = '../data/50000.txt'
 GT_REPRESENTATIVE_FILE_PATH = '../data/1000N_6K_gt_representative.txt'
+
 
 # Read ground truth representative
 if GT_REPRESENTATIVE_FILE_PATH != '': 
@@ -25,20 +26,25 @@ if GT_REPRESENTATIVE_FILE_PATH != '':
 else:
     gt_representative = []
 
-ALL_FEATURES = feature.get_all_features(DIMENSION, ALL_FEATURE_FILE_PATH, gt_representative)
+all_features = feature.get_all_features(DIMENSION, ALL_FEATURE_FILE_PATH, gt_representative)
 
 # Calling the library
-data, ground_truth_labels, representatives = dg.generate(
+data, ground_truth_labels, representatives, overlap_percentage = dg.generate(
     DATA_SIZE, 
     SIZE_OF_CLUSTERS, 
     NUMBER_OF_CLUSTER, 
     DIMENSION, 
     DISTANCE_THRESHOLD, 
     SIZE_OF_SET,
-    ALL_FEATURES,
+    all_features,
     gt_representative)
 
-data_file_name = '../out/gen_data.txt'
-data_utilities.write_file(data, data_file_name)
-data_utilities.write_file(representatives, '../out/gen_representative.txt')
-np.savetxt('../out/gen_ground_truth_labels.txt', ground_truth_labels.T, fmt='%d') 
+print('--- overlap percentage is:', overlap_percentage)
+
+OUT_DATA_FILE_NAME = '../out/gen_data{}N_{}K_{}overlap.txt'.format(DATA_SIZE, NUMBER_OF_CLUSTER, overlap_percentage)
+OUT_GT_REPRESENTATIVE_FILE_NAME = '../out/gt_representative{}N_{}K_{}overlap.txt'.format(DATA_SIZE, NUMBER_OF_CLUSTER, overlap_percentage)
+OUT_GT_LABELS_FILE_NAME = '../out/gt_labels{}N_{}K_{}overlap.txt'.format(DATA_SIZE, NUMBER_OF_CLUSTER, overlap_percentage)
+
+data_utilities.write_file(data, OUT_DATA_FILE_NAME)
+data_utilities.write_file(representatives, OUT_GT_REPRESENTATIVE_FILE_NAME)
+np.savetxt(OUT_GT_LABELS_FILE_NAME, ground_truth_labels.T, fmt='%d') 
